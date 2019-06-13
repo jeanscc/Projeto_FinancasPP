7bef8af7cc8896610b4f99661bbe1339ee0063a1
Jean <jeanmateusb13@gmail.com>
2019-06-13 15:27:10 -0300

    Criação da Tela de Login
    Criação da tela de login no pacote views.fxml e do seu respectivo controller no pacote view.controllers. Criação do pacote e da classe main.

---
+++ Projeto_Financas_PP/src/main/java/view/controllers/ControllerLogin.java
@@ -0,0 +1,65 @@
+package view.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ControllerLogin implements Initializable{
+
+    @FXML
+    private TextField txEmail;
+
+    @FXML
+    private PasswordField txSenha;
+
+    @FXML
+    private Button btLogin;
+
+    @FXML
+    private Button btCadastrar;
+
+    @FXML
+    private Button btForgot;
+
+    @FXML
+    void lsCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lsExit(MouseEvent event) {
+    	((Button) event.getPickResult().getIntersectedNode()).setCursor(Cursor.DEFAULT);;
+    }
+
+    @FXML
+    void lsFocus(MouseEvent event) {
+    	((Button) event.getPickResult().getIntersectedNode()).setUnderline(true);
+    	((Button) event.getPickResult().getIntersectedNode()).setCursor(Cursor.HAND);;
+    	
+    }
+
+    @FXML
+    void lsForgot(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lsLogin(ActionEvent event) {
+
+    }
+
+	public void initialize(URL location, ResourceBundle resources) {
+		txEmail.setStyle("-fx-text-fill: lightgray; -fx-font-size: 12px;");
+		txSenha.setStyle("-fx-text-fill: lightgray; -fx-font-size: 12px;");
+		
+	}
+
+}
