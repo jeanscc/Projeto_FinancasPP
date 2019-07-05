3216ca70738a86e43ea0b6d314cb37ee2492aa17
Jean <jeanmateusb13@gmail.com>
2019-07-05 17:14:15 -0300

    ControlerConsulta

--- Projeto_Financas_PP/src/main/java/view/controllers/ControllerConsultas.java
+++ Projeto_Financas_PP/src/main/java/view/controllers/ControllerConsultas.java
@@ -10,46 +10,63 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
+import control.ControlerMoeda;
+import control.ValorControl;
+import dto.MoedaDTO;
+import iterator.Iterator;
+import iterator.IteratorMoeda;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.chart.CategoryAxis;
+
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
+
 import javafx.scene.chart.XYChart;
-import javafx.scene.chart.XYChart.Series;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import model.requisicoes.ITrequeste;
-import model.requisicoes.RequisicaoBitCoin;
 import model.requisicoes.RequisicaoDolar;
 
 public class ControllerConsultas implements Initializable{
 	private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-	private CategoryAxis linhaX = new CategoryAxis();
-	private NumberAxis linhaY = new NumberAxis();
 	private ScheduledExecutorService scheduledExecutorService;
-	private ITrequeste consulta = new RequisicaoBitCoin();
+	private ITrequeste consulta = new RequisicaoDolar();
 	private Double num = new Double(0);
 	
-    @FXML
+	@FXML
     private Pane pnlInvestimentos;
+	
+	@FXML
+    private Label lbValor;
+	
+	@FXML
+    private Label lbValorH;
 
     @FXML
-    private ComboBox<?> cbTipo;
-
-    @FXML
-    private ComboBox<?> cbInvestimento;
-
-    @FXML
-    private LineChart<String, Number> grConsulta = new LineChart<String, Number>(linhaX,linhaY);
+    private ComboBox<MoedaDTO> cbInvestimento;
 
     @FXML
     private Button btConsultar;
-    
+
     @FXML
     private Button btParar;
+
+    @FXML
+    private LineChart<String, Number> grConsulta;
+
+    @FXML
+    private ComboBox<MoedaDTO> cbMoeda;
+
+    @FXML
+    private Button btHistorico;
+
+    @FXML
+    private LineChart<String, Number> grHistorico;
 
     @FXML
     void lsParar(ActionEvent event) {
@@ -57,11 +74,16 @@ public class ControllerConsultas implements Initializable{
     }
     
     @FXML
+    void lsHistorico(ActionEvent event) {
+    	
+    }
+    
+    @FXML
     void lsConsultar(ActionEvent event) {
     	int var = 10;
     	
     	final XYChart.Series<String, Number> series = new XYChart.Series<>();
-    	series.setName("Consultando");
+    	series.setName("Valor");
     	grConsulta.getData().add(series);
     	
     	
@@ -86,14 +108,39 @@ public class ControllerConsultas implements Initializable{
             Date now = new Date();
                 // put random number with current time
               series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), num));
+              lbValor.setText(String.valueOf(num));
               if (series.getData().size() > var)
                   series.getData().remove(0);
             });
         }, 0, 1, TimeUnit.SECONDS);
     }
+    
+    public void carregarCb(){
+    	ControlerMoeda controler= new ControlerMoeda();
+		try {
+			MoedaDTO m = controler.listar();
+			ObservableList <MoedaDTO> registro = FXCollections.observableArrayList();
+			Iterator it = new IteratorMoeda(m.getTodasMoedas());
+			while(it.hasNext()) {
+				registro.add((MoedaDTO) it.next());
+			}
+		
+			cbInvestimento.setItems(registro);
+			cbMoeda.setItems(registro);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 
+    
+    public void carregarGrafico(MoedaDTO moeda) {
+    	ValorControl controler = new ValorControl();
+    	
+    }
+    
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		carregarCb();
 		
 	}
 
