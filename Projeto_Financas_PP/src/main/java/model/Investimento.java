3a4759ed843ba497f3672521ea5d4d99aac43cd1
Victor <feraoofera@hotmail.com>
2019-07-05 15:37:40 -0300

    att investimento

--- Projeto_Financas_PP/src/main/java/model/Investimento.java
+++ Projeto_Financas_PP/src/main/java/model/Investimento.java
@@ -10,6 +10,8 @@ import daoJPA.InvestimentoDaoJPA;
 
 import dto.InvestimentoDTO;
 import dto.LucroDTO;
+import dto.ValorAtualDTO;
+import model.requisicoes.FachadaRequisicoes;
 
 
 
@@ -156,4 +158,177 @@ public class Investimento {
 
     }
 	
+	public void atualizarInvestimentos() {
+		InvestimentoDaoJPA invDao = new InvestimentoDaoJPA();
+		InvestimentoDTO investimento = null;
+		try{
+			investimento = invDao.listar();
+		}catch(Exception e) {
+			
+		}
+		FachadaRequisicoes fachada = new FachadaRequisicoes();
+		ValorAtualDTO valor = fachada.recuperarRequisicoes();
+		Date data = new Date();
+		
+		for(InvestimentoDTO inv:investimento.getTodosInvetismento()) {
+			switch (inv.getMoeda().getNome()) {
+				case "BCash":
+					double valorlucro = inv.getValor() * Double.parseDouble(valor.getBCash()) - inv.getValor(); 
+					List<LucroDTO> lucro = inv.getLucro();
+					LucroDTO lucrodto = null;
+					for(LucroDTO l : lucro) {
+						lucrodto = l;
+					}
+					lucrodto.setData(data);
+					lucrodto.setValor(valorlucro);
+					lucro.add(lucrodto);
+					inv.setLucro(lucro);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "BitCoin":
+					double valorlucro2 = inv.getValor() * Double.parseDouble(valor.getBitCoin()) - inv.getValor(); 
+					List<LucroDTO> lucro2= inv.getLucro();
+					LucroDTO lucrodto2 = null;
+					for(LucroDTO l : lucro2) {
+						lucrodto2 = l;
+					}
+					lucrodto2.setData(data);
+					lucrodto2.setValor(valorlucro2);
+					lucro2.add(lucrodto2);
+					inv.setLucro(lucro2);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "Dolar":
+					double valorlucro3 = inv.getValor() * Double.parseDouble(valor.getDolar()) - inv.getValor(); 
+					List<LucroDTO> lucro3= inv.getLucro();
+					LucroDTO lucrodto3 = null;
+					for(LucroDTO l : lucro3) {
+						lucrodto3 = l;
+					}
+					lucrodto3.setData(data);
+					lucrodto3.setValor(valorlucro3);
+					lucro3.add(lucrodto3);
+					inv.setLucro(lucro3);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "DolarCan":
+					double valorlucro4 = inv.getValor() * Double.parseDouble(valor.getDolarCan()) - inv.getValor(); 
+					List<LucroDTO> lucro4= inv.getLucro();
+					LucroDTO lucrodto4 = null;
+					for(LucroDTO l : lucro4) {
+						lucrodto4 = l;
+					}
+					lucrodto4.setData(data);
+					lucrodto4.setValor(valorlucro4);
+					lucro4.add(lucrodto4);
+					inv.setLucro(lucro4);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "Ethereum":
+					double valorlucro6 = inv.getValor() * Double.parseDouble(valor.getEthereum()) - inv.getValor(); 
+					List<LucroDTO> lucro6= inv.getLucro();
+					LucroDTO lucrodto6 = null;
+					for(LucroDTO l : lucro6) {
+						lucrodto6 = l;
+					}
+					lucrodto6.setData(data);
+					lucrodto6.setValor(valorlucro6);
+					lucro6.add(lucrodto6);
+					inv.setLucro(lucro6);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "Euro":
+					double valorlucro7 = inv.getValor() * Double.parseDouble(valor.getEuro()) - inv.getValor(); 
+					List<LucroDTO> lucro7= inv.getLucro();
+					LucroDTO lucrodto7 = null;
+					for(LucroDTO l : lucro7) {
+						lucrodto7 = l;
+					}
+					lucrodto7.setData(data);
+					lucrodto7.setValor(valorlucro7);
+					lucro7.add(lucrodto7);
+					inv.setLucro(lucro7);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "Libra":
+					double valorlucro8 = inv.getValor() * Double.parseDouble(valor.getLibra()) - inv.getValor(); 
+					List<LucroDTO> lucro8= inv.getLucro();
+					LucroDTO lucrodto8 = null;
+					for(LucroDTO l : lucro8) {
+						lucrodto8 = l;
+					}
+					lucrodto8.setData(data);
+					lucrodto8.setValor(valorlucro8);
+					lucro8.add(lucrodto8);
+					inv.setLucro(lucro8);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "LiteCoin":
+					double valorlucro9 = inv.getValor() * Double.parseDouble(valor.getLiteCoin()) - inv.getValor(); 
+					List<LucroDTO> lucro9= inv.getLucro();
+					LucroDTO lucrodto9 = null;
+					for(LucroDTO l : lucro9) {
+						lucrodto9 = l;
+					}
+					lucrodto9.setData(data);
+					lucrodto9.setValor(valorlucro9);
+					lucro9.add(lucrodto9);
+					inv.setLucro(lucro9);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+				case "XRP":
+					double valorlucro0 = inv.getValor() * Double.parseDouble(valor.getXRP()) - inv.getValor(); 
+					List<LucroDTO> lucro0= inv.getLucro();
+					LucroDTO lucrodto0 = null;
+					for(LucroDTO l : lucro0) {
+						lucrodto0 = l;
+					}
+					lucrodto0.setData(data);
+					lucrodto0.setValor(valorlucro0);
+					lucro0.add(lucrodto0);
+					inv.setLucro(lucro0);
+					try{
+						invDao.atualizar(inv);
+					}catch(Exception e){
+						
+					}
+				break;
+			}
+			
+			
+		}
+		
+	}
 }
